31b0d6b7c20ac77bd165d499d60436bf051c1d63
Marcos Gallardo Pérez <marcos.gallardo@atmira.com>
2019-03-21 13:34:02 +0100

    HeaderComponent

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eit-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent implements OnInit {
+  public usuario: string;
+  constructor() { }
+
+  ngOnInit() {
+    this.usuario = 'Marcos';
+  }
+
+}
